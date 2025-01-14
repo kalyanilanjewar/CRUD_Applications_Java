381f51ce83313bf252cb13a095b4caade79544e6
Kalyani <kblanjewar024@gmail.com>
2025-01-14 19:46:07 +0530

    added lombok annotations to dto

--- src/main/java/com/simpleJavaCrud/crud/dto/EmployeeDto.java
+++ src/main/java/com/simpleJavaCrud/crud/dto/EmployeeDto.java
@@ -2,11 +2,15 @@ package com.simpleJavaCrud.crud.dto;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor //Lombok annotation to generate no-args constructor
+@AllArgsConstructor // Lombok annotation to generate all-args constructor
 public class EmployeeDto {
 	
 	private String id;
